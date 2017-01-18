eee92c23b9f10d2c780c89144a46899ed4d5d540
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-01-18 12:16:34 +0100

    Actualizado ficheros para añadir el proyecto a Heroku

--- LTNews/wsgi.py
+++ LTNews/wsgi.py
@@ -8,15 +8,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-import sys
-from django.core.wsgi import get_wsgi_application
 
-path = '/home/andjimrio/LTN'  # aquí utiliza tu propio usuario, sin los simbolos < y >
-if path not in sys.path:
-    sys.path.append(path)
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LTNews.settings")
 
 application = get_wsgi_application()
-
-

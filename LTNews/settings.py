b1ea8e1a540baf8f0d3c8e1fdc57dbda4b0be9ee
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-01-19 13:30:45 +0100

    Organización del directorio static

--- LTNews/settings.py
+++ LTNews/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROY_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,5 +127,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(PROY_DIR,'static'),
 )

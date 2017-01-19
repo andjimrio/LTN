a43b8cbf76ce449f102d2f4324236a1747400c9a
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-01-19 13:22:41 +0100

    Camdiaba estrcutura del proyecto: archivos subidos, gitignore...

--- LTNews/settings.py
+++ LTNews/settings.py
@@ -51,7 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'LTNews.urls'
+
+# Login static configuration
 LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_URL = '/'
 
 TEMPLATES = [
     {
@@ -121,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)

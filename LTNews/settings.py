1c0229d26c717de464453a751cc427b1027390f4
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-01-19 13:43:08 +0100

    Añadida línea de static_root a settings

--- LTNews/settings.py
+++ LTNews/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROY_DIR,'static'),

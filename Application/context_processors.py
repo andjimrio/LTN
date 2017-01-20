bcbb697b4b14009b1fd6f2227bec1627070395d3
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-01-20 13:00:00 +0100

    Rehecha la plantilla inicial: se ha pasado de MDL a Materialize.
    Google hace muy bien muchas cosas. Se ve que los frameworks de CSS no son lo suyo.

---
+++ Application/context_processors.py
@@ -0,0 +1,2 @@
+def color_pallete(request):
+    return {'color_primary':'red','color_secondary':'grey'}
---
+++ LTNews/static/js/init.js
@@ -0,0 +1,10 @@
+/**
+ * Created by andre on 20/01/2017.
+ */
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space

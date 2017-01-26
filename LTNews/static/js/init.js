8c5c326e5a6aacd21c989ab0f625a27ac195078a
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-01-26 19:27:23 +0100

    Article hecho
    Habría que repensar el model en vez de article

--- LTNews/static/js/init.js
+++ LTNews/static/js/init.js
@@ -4,6 +4,6 @@
 (function($){
     $(function(){
         $('.button-collapse').sideNav();
-
+        $('.parallax').parallax();
     }); // end of document ready
 })(jQuery); // end of jQuery name space

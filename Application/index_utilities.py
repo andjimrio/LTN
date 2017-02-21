93380473349ebf70affa225864296a74e9f9ad97
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-02-21 20:05:58 +0100

    Refactorizado las utilidades del índice

--- Application/index_utilities.py
+++ Application/index_utilities.py
@@ -1,32 +1,49 @@
 from whoosh import index
 from whoosh.qparser import QueryParser
-from os.path import join,dirname,abspath
+from os.path import join,dirname,abspath,exists
 
 PROJECT_DIR = dirname(dirname(abspath(__file__)))
 INDEX = join(PROJECT_DIR,"LTNews/whoosh_index")
 
-def keywords(id):
-    ix = index.open_dir(INDEX)
-    parser = QueryParser("ide", ix.schema)
-    myquery=parser.parse(str(id))
-    with ix.searcher() as searcher:
-        results=searcher.search(myquery)
-        keywords = [keyword for keyword, score
-                in results.key_terms("article", numterms=20)]
+def get_index():
+    if not exists(INDEX):
+        ix = False
+    else:
+        ix = index.open_dir(INDEX)
+    return ix
+
+def query_search(ix,field,search):
+    query = QueryParser(field, ix.schema).parse(search)
+    results = ix.searcher().search(query)
+    return results
+
+def close_query(ix):
     ix.searcher().close()
+    pass
+
+def keywords(id):
+    ix = get_index()
+    if ix == False:
+        return []
+    results = query_search(ix, "ide", str(id))
+    keywords = [keyword for keyword, score
+                in results.key_terms("article", numterms=20)]
+    close_query(ix)
+
     return keywords
 
 def identity(id):
-    ix = index.open_dir(INDEX)
-    parser = QueryParser("ide", ix.schema)
-    myquery=parser.parse(str(id))
-    with ix.searcher() as searcher:
-        results=searcher.search(myquery)
-        first_hit = results[0]
-        more_results = [more['ide'] for more in first_hit.more_like_this("article")]
-    ix.searcher().close()
+    ix = get_index()
+    if ix == False:
+        return []
+    results = query_search(ix, "ide", str(id))
+    first_hit = results[0]
+    more_results = [more['ide'] for more in first_hit.more_like_this("article")]
+    close_query(ix)
+
     return more_results
+
 
 if __name__ == '__main__':
     print(keywords(70))
-    print(identity(10))
+    print(identity(72))

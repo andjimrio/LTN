3ac2a7f60f28a5053c5777524d3fa9d5e4b8cc67
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-05-14 16:35:40 +0200

    draft #59: añadido internacionalización

--- LTNews/static/js/init.js
+++ LTNews/static/js/init.js
@@ -9,3 +9,16 @@
         $('select').material_select();
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+$( document ).ready(function() {
+    $("#flag").click(function() {
+        var lang = Cookies.get('lang');
+        if(lang == 'es-es') {
+            Cookies.set('lang', 'en-us');
+        } else {
+            Cookies.set('lang', 'es-es');
+        }
+        location.reload(true);
+    });
+});

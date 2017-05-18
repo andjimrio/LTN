cf5fb891444da449ec51dc8f127e601a126b4096
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2017-05-18 21:04:54 +0200

    doble comillas en init

--- LTNews/static/js/init.js
+++ LTNews/static/js/init.js
@@ -2,17 +2,17 @@
  * Created by andre on 20/01/2017.
  */
 $( document ).ready(function() {
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('.modal').modal();
-    $('select').material_select();
+    $(".button-collapse").sideNav();
+    $(".parallax").parallax();
+    $(".modal").modal();
+    $("select").material_select();
 
     $("#flag").click(function() {
-        var lang = Cookies.get('lang');
-        if(lang === 'es-es') {
-            Cookies.set('lang', 'en-us');
+        var lang = Cookies.get("lang");
+        if(lang === "es-es") {
+            Cookies.set("lang", "en-us");
         } else {
-            Cookies.set('lang', 'es-es');
+            Cookies.set("lang", "es-es");
         }
         location.reload(true);
     });
